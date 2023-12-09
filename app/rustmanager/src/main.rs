1f5b37d6c75af6f7d53c106432db0e9894ea6edb
Niks Martinsons <66245053+nmartinsons@users.noreply.github.com>
2023-12-09 14:40:10 +0200

    Added layout of the functions for the app

--- app/rustmanager/src/main.rs
+++ app/rustmanager/src/main.rs
@@ -1,14 +1,46 @@
 use rusqlite::{Connection, Result};
 
-fn main(){
-    if let Err(e) = connection() {
-        eprintln!("Error establishing connection: {}", e);
-    }
-    
-}
 
 fn connection() -> Result<()>{
     let _conn = Connection::open("RustData.db")?;
     println!("Connection established successfully!");
     Ok(())
+}
+
+
+//ability to create tasks
+fn createData(){
+    
+}
+
+
+//ability to view tasks
+fn viewData(){
+    
+}
+
+
+//ability to update data
+fn updateData(){
+    
+}
+
+
+//ability to delete data
+fn deleteData(){
+    
+}
+
+
+//ablity to see number of incomplete tasks
+fn viewIncompleteTasks(){
+
+}
+
+
+fn main(){
+    if let Err(e) = connection() {
+        eprintln!("Error establishing connection: {}", e);
+    }
+    
 }
